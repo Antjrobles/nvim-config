0c15ef37e9a235c2dd6400387b63d34cf83bfaf9
Antonio Robles <antjrobles@gmail.com>
2025-05-16 11:38:13 +0200

    Añade el plugin nvim-autopairs para autocompletar paréntesis y corchetes con configuración personalizada

--- lua/plugins/more_plugins.lua
+++ lua/plugins/more_plugins.lua
@@ -11,4 +11,31 @@ return {
       -- Mapeos simples como respaldo (similares a <leader>m
     end,
   },
+  -- Plugin 25: windwp/nvim-autopairs (Autocompletar paréntesis y corchetes)
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = true, -- Habilitar soporte para TypeScript
+        disable_filetype = { "TelescopePrompt", "vim" },
+        fast_wrap = {
+          map = "<M-e>",
+          chars = { "{", "[", "(", '"', "'" },
+          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+          end_key = "$",
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          check_comma = true,
+          highlight = "Search",
+        },
+        ts_node = {
+          ["@function.outer"] = { "{", "}" },
+          ["@class.outer"] = { "{", "}" },
+          ["@conditional.outer"] = { "{", "}" },
+          ["@loop.outer"] = { "{", "}" },
+          ["@parameter.outer"] = { "{", "}" },
+        },
+      })
+    end,
+  },
 }
